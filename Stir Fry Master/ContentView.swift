cfaf1b071865b77596720230e270ee29ea7440d4
Aaron Southammavong <aaroneight07@gmail.com>
2020-07-02 22:00:00 -0500

    initial commit

---
+++ Stir Fry Master/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stir Fry Master
+//
+//  Created by Aaron Southammavong on 7/2/20.
+//  Copyright © 2020 Aaron Southammavong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stir Fry MasterTests/Stir_Fry_MasterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stir_Fry_MasterTests.swift
+//  Stir Fry MasterTests
+//
+//  Created by Aaron Southammavong on 7/2/20.
+//  Copyright © 2020 Aaron Southammavong. All rights reserved.
+//
+
+import XCTest
+@testable import Stir_Fry_Master
+
+class Stir_Fry_MasterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
